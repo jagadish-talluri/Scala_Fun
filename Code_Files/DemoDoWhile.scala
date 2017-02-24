df48cd91bd24c0b45528ef58bb94a599187babb8
Jagadish Talluri <jagadish-talluri@users.noreply.github.com>
2017-02-24 14:01:23 +0530

    Create DemoDoWhile.scala

---
+++ Code_Files/DemoDoWhile.scala
@@ -0,0 +1,12 @@
+object DemoDoWhile {
+    def main(args: Array[String]) {
+        var i = 10
+        
+        do{
+        
+        i += 1
+        println("The Value is : " + i)
+        
+        }while( i < 20 ) 
+    }
+}
